2e4dabfd5db3ec99e6974d52a8de951060f50fc0
renanldev <renanlimasouza03@gmail.com>
2026-04-17 09:49:22 -0300

    atualizacao do backend

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '15mb' }));
+app.use(express.urlencoded({ extended: true, limit: '15mb' }));
 
 app.get('/', (req, res) => {
   res.json({ message: 'API de Projetos Acadêmicos funcionando.' });
